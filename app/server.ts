2c70905754e695c9c5a47249459f375d15a0381e
Wojcirej <wojciech.bal@epoczta.pl>
2020-03-01 18:44:25 +0100

    Added middleware parsing JSON request body.

--- app/server.ts
+++ app/server.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import morgan from "morgan";
+import bodyParser from "body-parser";
 
 import { router as defaultRouter } from "./routes/default";
 import { router as healthCheckRouter } from "./routes/healthcheck";
@@ -7,6 +8,7 @@ import { router as healthCheckRouter } from "./routes/healthcheck";
 const app = express();
 
 app.use(morgan("combined"));
+app.use(bodyParser.json());
 
 app.use("/app_status", healthCheckRouter);
 app.use("*", defaultRouter);
