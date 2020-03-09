4feb2ed0d2b308f51c7f46338f9249322c7bf19c
Wojcirej <wojciech.bal@epoczta.pl>
2020-03-09 22:22:13 +0100

    Introduce find and findAll methods to InvoiceRepository.

--- domain/Invoices/repositories/InvoiceRepository.ts
+++ domain/Invoices/repositories/InvoiceRepository.ts
@@ -1,10 +1,47 @@
 import fs from "fs";
 import path from "path";
 import { Invoice } from "../Invoice";
+import { CompanyFactory } from "../factories/CompanyFactory";
+import { OrderFactory } from "../factories/OrderFactory";
+import { InvoiceNotFoundError } from "../errors/InvoiceNotFoundError";
 
 export class InvoiceRepository {
+  private readonly path: string;
+
+  constructor() {
+    this.path = path.join(process.cwd(), "/tmp/db/invoices");
+  }
+
   save(invoice: Invoice): boolean {
-    fs.writeFileSync(path.join(process.cwd(), `/tmp/db/invoices/${invoice.id}.json`), JSON.stringify(invoice, null, 2));
+    fs.writeFileSync(`${this.path}/${invoice.id}.json`, JSON.stringify(invoice, null, 2));
     return true;
+  }
+
+  find(invoiceId: string): Invoice {
+    const all = this.findAll();
+    const invoice = all.find(invoice => invoice.id === invoiceId);
+    if (!invoice) {
+      throw new InvoiceNotFoundError(`Invoice with id ${invoiceId} does not exist.`);
+    }
+    return invoice;
+  }
+
+  findAll(): Array<Invoice> {
+    const all = fs.readdirSync(this.path);
+    return all.map(invoiceEntry => {
+      const invoice = JSON.parse(fs.readFileSync(`${this.path}/${invoiceEntry}`).toString());
+      return new Invoice({
+        invoiceDetails: {
+          id: invoice.id,
+          status: invoice.status,
+          issuedAt: invoice.issuedAt,
+          invoiceNumber: invoice.invoiceNumber,
+          saleDate: invoice.saleDate
+        },
+        buyer: CompanyFactory.build(invoice.buyer),
+        seller: CompanyFactory.build(invoice.seller),
+        order: OrderFactory.build(invoice.order.orderRows)
+      });
+    });
   }
 }
--- spec/unit/domain/InvoiceRepository.spec.ts
+++ spec/unit/domain/InvoiceRepository.spec.ts
@@ -1,6 +1,7 @@
 import fs from "fs";
 import path from "path";
 
+import { Invoice } from "../../../domain/Invoices/Invoice";
 import { InvoiceRepository } from "../../../domain/Invoices/repositories/InvoiceRepository";
 import { InvoiceFactory } from "../../../domain/Invoices/factories/InvoiceFactory";
 import { invoicePayload } from "../../support/mocks/payloadSamples";
@@ -19,6 +20,42 @@ describe("InvoiceRepository", () => {
       it("saves passed object as JSON file in specified directory", () => {
         repository.save(invoice);
         expect(fs.existsSync(path.join(process.cwd(), `/tmp/db/invoices/${invoice.id}.json`))).toBe(true);
+      });
+    });
+  });
+
+  describe("#findAll", () => {
+    it("returns list of the Invoice objects", () => {
+      const allInvoices = repository.findAll();
+      expect(allInvoices.every(invoice => invoice instanceof Invoice)).toBe(true);
+    });
+  });
+
+  describe("#find", () => {
+    describe("when Invoice with provided ID exists", () => {
+      const invoiceId = "0a0c0a14-c537-44bb-9716-5e181a47d977";
+
+      it("returns Invoice instance", () => {
+        const invoice = repository.find(invoiceId);
+        expect(invoice).toBeInstanceOf(Invoice);
+      });
+
+      it("returns Invoice instance with provided ID", () => {
+        const invoice = repository.find(invoiceId);
+        expect(invoice.id).toEqual(invoiceId);
+      });
+    });
+
+    describe("when Invoice with provided ID does not exist", () => {
+      const invoiceId = "0";
+
+      it("throws InvoiceNotFoundError", () => {
+        try {
+          repository.find(invoiceId);
+        } catch (error) {
+          expect(error.name).toEqual("InvoiceNotFoundError");
+          expect(error.message).toEqual(`Invoice with id ${invoiceId} does not exist.`);
+        }
       });
     });
   });
